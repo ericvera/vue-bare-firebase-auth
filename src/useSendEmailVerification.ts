094fe4d65d0373f0fe5f97b6c9aea5656ce85786
Eric Vera <6858503+ericvera@users.noreply.github.com>
2025-01-29 17:50:30 -0400

    fix: update import in useSendEmailVerification, upgrade dependencies, and fix externals

--- src/useSendEmailVerification.ts
+++ src/useSendEmailVerification.ts
@@ -1,6 +1,6 @@
 import { storeToRefs } from 'pinia'
 import { readonly, ref, watch } from 'vue'
-import { useAuthStore } from 'vue-bare-firebase-auth'
+import { useAuthStore } from './useAuthStore.js'
 
 type SendEmailVerificationResult = 'link-sent' | 'email-verified' | undefined
 
